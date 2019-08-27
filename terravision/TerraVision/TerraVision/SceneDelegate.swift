0e7e5123a333f7c3e1f4c5f5e1565c9fada88f42
Arunabh Das <arunabhdas@gmail.com>
2019-08-27 15:11:34 -0400

    Making UsersViewController rootViewController

--- terravision/TerraVision/TerraVision/SceneDelegate.swift
+++ terravision/TerraVision/TerraVision/SceneDelegate.swift
@@ -24,7 +24,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            
+            // window.rootViewController = UIHostingController(rootView: contentView)
+            
+            window.rootViewController = UsersViewController()
             self.window = window
             window.makeKeyAndVisible()
         }
